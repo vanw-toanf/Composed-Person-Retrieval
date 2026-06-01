e3c4bb099dc7e1f0c00c1d84a0369b5da2b6377b
vanwtoanf <damtoan321@gmail.com>
2026-06-01 19:01:49 +0700

    edit update torch

--- FAFA_SynCPR/src/lavis/models/blip2_models/Qformer.py
+++ FAFA_SynCPR/src/lavis/models/blip2_models/Qformer.py
@@ -38,10 +38,13 @@ from transformers.modeling_outputs import (
 )
 from transformers.modeling_utils import (
     PreTrainedModel,
-    apply_chunking_to_forward,
     find_pruneable_heads_and_indices,
     prune_linear_layer,
 )
+try:
+    from transformers.modeling_utils import apply_chunking_to_forward
+except ImportError:
+    from transformers.pytorch_utils import apply_chunking_to_forward
 from transformers.utils import logging
 from transformers.models.bert.configuration_bert import BertConfig
 
