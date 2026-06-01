53e37bcefdc877d067e6ee172a4deb3c5142ebb0
vanwtoanf <damtoan321@gmail.com>
2026-06-01 19:05:45 +0700

    edit version

--- FAFA_SynCPR/src/lavis/models/blip2_models/Qformer.py
+++ FAFA_SynCPR/src/lavis/models/blip2_models/Qformer.py
@@ -36,15 +36,19 @@ from transformers.modeling_outputs import (
     SequenceClassifierOutput,
     TokenClassifierOutput,
 )
-from transformers.modeling_utils import (
-    PreTrainedModel,
-    find_pruneable_heads_and_indices,
-    prune_linear_layer,
-)
+from transformers.modeling_utils import PreTrainedModel
 try:
-    from transformers.modeling_utils import apply_chunking_to_forward
+    from transformers.modeling_utils import (
+        apply_chunking_to_forward,
+        find_pruneable_heads_and_indices,
+        prune_linear_layer,
+    )
 except ImportError:
-    from transformers.pytorch_utils import apply_chunking_to_forward
+    from transformers.pytorch_utils import (
+        apply_chunking_to_forward,
+        find_pruneable_heads_and_indices,
+        prune_linear_layer,
+    )
 from transformers.utils import logging
 from transformers.models.bert.configuration_bert import BertConfig
 
